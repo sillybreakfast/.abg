3c3989bb72e4fe2bc770b7f99311b67c635d6342
qwertyy <qwertyytheartist@gmail.com>
2024-06-02 10:59:38 -0700

    overhaul. rewrote lots of code. fix #1

--- abg.py
+++ abg.py
@@ -1,41 +1,21 @@
 # acsii-based graphics format
 # version 1.0.1
 
-file = open(input("open a file... "), "r").read()
+file = open(input("open a file... "), "r")
 
 class abg:
 	def interpret(file):
 		try:
-			lines = file.splitlines()
+			lines = file.read().splitlines()
 
-			if lines[0] == "1.0.1":
-				brightnesslevels = ["1", "2", "3", "4", "x"]
-				brightness = ["  ", "░░", "▓▓", "██", "\033[0m  \033[40m"]
-				colornames = ["r", "g", "y", "v", "p", "b", "w", "x"]
-				colors = ["\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m", "\033[37m", ""]
-				result = "\033[40m"
-				for y in range(len(lines)-2):
-					linepixels = lines[y+2].split(" ")
-					for x in range(len(linepixels)):
-						pixel = linepixels[x]
-						if pixel[0].lower() in colornames:
-							result += colors[colornames.index(pixel[0].lower())]
-							if pixel[1].lower() in brightnesslevels:
-								result += brightness[brightnesslevels.index(pixel[1].lower())]
-							else:
-								raise ValueError("no such brightness level \"%s\"", pixel[1].lower())
-						else:
-							raise ValueError("no such color \"%s\"", pixel[0].lower())
-					result += "\n"
-				return result
-			else if lines[0] == "1.1.2":
+			if file.name.lower().endswith(".abg"):
 				brightnesslevels = ["1", "2", "3", "4", "x"]
 				brightness = ["  ", "░░", "▓▓", "██", "\033[0m  \033[40m"]
 				colornames = ["r", "g", "y", "c", "p", "b", "w", "x"]
 				colors = ["\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m", "\033[37m", ""]
 				result = "\033[40m"
-				for y in range(len(lines)-2):
-					linepixels = lines[y+2].split(" ")
+				for y in range(len(lines)-1):
+					linepixels = lines[y+1].split(" ")
 					for x in range(len(linepixels)):
 						pixel = linepixels[x]
 						if pixel[0].lower() in colornames:
@@ -43,17 +23,14 @@ class abg:
 							if pixel[1].lower() in brightnesslevels:
 								result += brightness[brightnesslevels.index(pixel[1].lower())]
 							else:
-								raise ValueError("no such brightness level \"%s\"", pixel[1].lower())
+								return "invalid brightness level"
 						else:
-							raise ValueError("no such color \"%s\"", pixel[0].lower())
+							return "invalid color"
 					result += "\n"
 				return result
 			else:
-				raise ValueError("no such version \"%s\"", lines[0])
+				return "this is not a .abg file"
 		except AttributeError:
 			return "something went wrong!"
-
-	def version(file):
-		return file.splitlines()[0]
 
 print(abg.interpret(file))
